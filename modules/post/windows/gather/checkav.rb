d35bd268eed97d48348fea95248b846d31368a7a
WO4HAOREN <61932870+wo4haoren@users.noreply.github.com>
2020-03-20 06:07:33 +0800

    Update checkav.rb

--- modules/post/windows/gather/checkav.rb
+++ modules/post/windows/gather/checkav.rb
@@ -14,47 +14,46 @@ class MetasploitModule < Msf::Post
     end
                                                                                                        
     def run
-        print_status("Check if target OS is supported...")
-        print_status("Target OS is " + sysinfo['OS'])
+
+        print_status("1. Check running process...")
+        client.core.use( "powershell" ) if not client.ext.aliases.include?( "powershell" )
+        vendors = ['MpCmdRun','MSASCui','Malware','Antivirus','CrowdStrike','Dr.Web','Ad-Aware','Avast','AVG','Avira','BitDefender','ClamAV','DrWeb','eScan','Eset','F-Secure','FireEye','Fortinet','K7AntiVirus','Kaspersky','Kingsoft','Malwarebytes','McAfee','Nano','Palo','Panda','Qihoo','Rising','Sangfor','Sophos','Symantec','Tencent','TrendMicro','Webroot','ZoneAlarm']
+        result = client.powershell.execute_string({session_id:1,code:"ps|Where {$_.Description.length -gt 0 }| sort Description,ProcessName -Unique | select { '{0}.exe:{1}' -f $_.ProcessName, $_.Description}"})
+
+        result.split("\n").each do |line|
+            vendors.each do |v|
+                if line =~ /#{v}/i
+                    processName = line.split(":")[0]
+                    description = line.split(":")[1]
+                    print_error("\tAV process found : #{processName} -> #{description}")
+                    break
+                end
+            end
+        end
+
+        print_status("2. Query antivirusproduct wmi table...")
+        print_status("\tTarget OS is " + sysinfo['OS'])
 
         if sysinfo['OS'] !~ /Windows (XP|Vista|7|8|10)/
-          print_error("Only supported on Windows \(XP|Vista|7|8|10\)")
+          print_error("\tOnly supported on Windows \(XP|Vista|7|8|10\)")
           return
         end
-                                                                                         
-        print_good("Loading extapi... ")
-        client.core.use("extapi")
 
-        root = 'root\SecurityCenter2'
-        if sysinfo['OS'] =~ /Windows XP/
-            root = 'root\SecurityCenter'
-        end
-
-        begin
-            result = client.extapi.wmi.query("select displayName, productState from antivirusproduct",root)
-            rescue RuntimeError
-                raise
-            return
-        end
-  
-        if result.count == 0 
-            print_status("No AV found by wmi query.")
-            return
-        end
-
-        result.each { |k,v|
-            if "#{k}" == "values"
-                    print_error("#{v.count} AV found!")
-                    v.each { |a,b|
-                        isEnable = sprintf("%06x", b).upcase[2,2].to_i < 10 ? "Disabled" : "Enabled"
-                        isUpToDate = sprintf("%06x", b).upcase[4,2] == "00" ? "UpToDate" : "not UpToDate" 
-                        if isEnable == "Enabled" 
-                            print_error("#{a} is #{isEnable} and the signature database is #{isUpToDate}")
-                        else
-                            print_status("#{a} is #{isEnable} and the signature database is #{isUpToDate}") 
-                        end
-                    }
+        result = client.powershell.execute_string({session_id:1,code:"Get-WmiObject -namespace 'root\\SecurityCenter2' -class antivirusproduct | select { '{0}:{1}' -f $_.displayName,$_.productState }|ft -hide"})
+        result.split("\n").each do |line|
+            displayName = line.split(":")[0]
+            state = line.split(":")[1]
+            if state == nil 
+                next
             end
-        }
+
+            isEnable = sprintf("%06x", state).upcase[2,2].to_i < 10 ? "Disabled" : "Enabled"
+            isUpToDate = sprintf("%06x", state).upcase[4,2] == "00" ? "UpToDate" : "not UpToDate" 
+            if isEnable == "Enabled" 
+                print_error("\t#{displayName} is #{isEnable} and the signature database is #{isUpToDate}")
+            else
+                print_status("\t#{displayName} is #{isEnable} and the signature database is #{isUpToDate}") 
+            end
+        end
     end
 end
