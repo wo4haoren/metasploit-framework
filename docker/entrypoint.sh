45d3b98d576868d41a9bb74c76868b7f872811a2
wo4haoren <wuwei201788@gmail.com>
2020-04-19 01:19:57 +0800

    Docker-compose file updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,7 @@ TMP=${MSF_GID:=1000}
 # if the user starts the container as root or another system user,
 # don't use a low privileged user as we mount the home directory
 if [ "$MSF_UID" -eq "0" ]; then
-  "$@"
+  screen "$@"
 else
   # if the users group already exists, create a random GID, otherwise
   # reuse it
@@ -23,9 +23,9 @@ else
     adduser -u $MSF_UID -D $MSF_USER -g $MSF_USER -G $MSF_GROUP $MSF_USER
     # add user to metasploit group so it can read the source
     addgroup $MSF_USER $METASPLOIT_GROUP
-    su-exec $MSF_USER "$@"
+    screen su-exec $MSF_USER "$@"
   # fall back to root exec if the user id already exists
   else
-    "$@"
+    screen "$@"
   fi
 fi
